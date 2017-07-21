98ca740f8bf7b964f142b889b8c795111848efad
R.Murzakov <romuald.murzakov@gmail.com>
2017-07-21 13:15:46 +0100

    Database and Tables for basic MVP are created

--- db/music_shop.sql
+++ db/music_shop.sql
@@ -1,13 +1,18 @@
 DROP TABLE albums;
 DROP TABLE artists;
 
-CREATE TABLE artists;
-id SERIAL8 PRIMARY KEY;
-name VARCHAR(255) ;
+
+CREATE TABLE artists
+  (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255)
+  );
 
 CREATE TABLE albums
-(
-id SERIAL8 PRIMARY KEY,
-name VARCHAR(255),
-artist_id INT8 REFERENCES artists.id
-)
+  (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  ideal_stock INT2,
+  current_stock INT2,
+  artist_id INT8 references artists(id)
+  );
