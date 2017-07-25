35728af54e110ca6f8acd2b02827f6cba339fb90
R.Murzakov <romuald.murzakov@gmail.com>
2017-07-25 23:19:36 +0100

    Stock management add function created

--- app.rb
+++ app.rb
@@ -1,12 +1,14 @@
 require('sinatra')
 require('sinatra/contrib/all') if development?
+require_relative('./models/artists')
+require_relative('./models/albums')
 
 #home page
 get '/' do
   erb(:index)
 end
 
-get '/stock_management' do
-  erb(:stock_management)
+get '/stock_management/new_album' do
+  erb( :"stock_management/stock_management" )
 end
 
