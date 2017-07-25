ec6be6e16b9d4e7015ad1adbb7f5f2f8f31c1b21
R.Murzakov <romuald.murzakov@gmail.com>
2017-07-25 22:56:08 +0100

    More artists & albums added

--- db/seed.rb
+++ db/seed.rb
@@ -9,8 +9,24 @@ Artist.delete_all
 
 artist1= Artist.new( {'name' => 'Linkin Park' } )
 artist1.save
+artist2= Artist.new( {'name' => 'Papa Roach' } )
+artist2.save
+artist3= Artist.new( {'name' => 'Queen' } )
+artist3.save
+artist4= Artist.new( {'name' => 'Metallica' } )
+artist4.save
+
+
 album1 = Album.new ( {'name' => 'Hyrbid Theory', 'ideal_stock' => '30', 'current_stock' => '20'} )
-p album1
 album1.save
+album2 = Album.new ( {'name' => 'Meteora', 'ideal_stock' => '30', 'current_stock' => '15'} )
+album2.save
+album3 = Album.new ( {'name' => 'Angels', 'ideal_stock' => '30', 'current_stock' => '22'} )
+album3.save
+album4 = Album.new ( {'name' => 'Deamons', 'ideal_stock' => '30', 'current_stock' => '16'} )
+album4.save
+
+
+
 binding.pry
 nil
--- models/artists.rb
+++ models/artists.rb
@@ -17,7 +17,11 @@ class Artist
     @id =  result[0]['id'].to_i
   end
 
-  
+  def self.all()
+    sql = "SELECT * FROM artists"
+    result = SqlRunner.run(sql)
+    return result.map {|hash| Artist.new(hash)}
+  end
 
   def self.delete_all()
     sql = "DELETE FROM artists;"
