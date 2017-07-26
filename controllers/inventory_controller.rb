5e341fdc66eb12e205a4725c7f7077cbc15a49ec
R.Murzakov <romuald.murzakov@gmail.com>
2017-07-26 14:45:58 +0100

    Inventory have been removed as unneccessary

--- controllers/inventory_controller.rb
+++
@@ -1,9 +0,0 @@
-require('sinatra')
-require('sinatra/contrib/all') if development?
-require_relative('../models/artist')
-require_relative('../models/album')
-
-get '/inventory' do
-  erb( :"inventory/index")
-
-end
