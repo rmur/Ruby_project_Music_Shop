86e70fb822cae26e7afc56cb9086ab31d68d0999
R.Murzakov <romuald.murzakov@gmail.com>
2017-07-26 10:48:31 +0100

    Create new artis is done

--- controllers/artists.rb
+++ controllers/artists.rb
@@ -5,14 +5,17 @@ require_relative('../models/albums')
 
 #index
 get '/artists' do
-
-erb(:'artists/index')
-
+  @artists = Artist.all
+  erb(:'artists/index')
 end
 
 #new_artist
 get '/artists/new' do
+  erb(:'artists/new')
+end
 
-erb(:'artists/new')
-
+post '/artists/new' do
+  @artist = Artist.new(params)
+  @artist.save
+  redirect '/artists'
 end
